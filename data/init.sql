9f1d7177b1fac95fdc3daca26145af43d08c0826
andre-rost <andrerost86@gmail.com>
2022-07-16 12:10:37 +0200

    Adding images

--- data/init.sql
+++ data/init.sql
@@ -5,3 +5,9 @@ CREATE TABLE "users" (
     date_joined DATE
 )
 
+INSERT INTO users (id, username, avatar, date_joined) VALUES
+    (1,'Homer','homer.png','2021-03-20'),
+    (2,'Marge','marge.png','2021-03-16'),
+    (3,'Bart','bart.png','2021-03-19'),
+    (4,'Lisa','lisa.png','2021-03-11'),
+    (5,'Maggie','maggie.png','2021-03-14')
